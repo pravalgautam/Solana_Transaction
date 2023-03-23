66942239301cf2d0bc980054455abc4356717861
pravalgautam <pravalgautam79@gmail.com>
2023-03-24 01:16:18 +0530

    change

--- token.js
+++ token.js
@@ -1,18 +1,19 @@
 const { Connection, PublicKey } = solanaWeb3;
 const form = document.querySelector("form");
 
-form.onsubmit = async () => {
-
+form.onsubmit = async (e) => {
+e.preventDefault()
    const amt = document.getElementById("Amount").value
 
   const publicKeyRecie = document.getElementById("publicKeyRecie").value;
 
   const connection = new Connection("https://api.devnet.solana.com");
 console.log(amt)
-const p= new PublicKey(publicKeyRecie);
+const publicKeyObject = new PublicKey(publicKeyRecie);
 
-let hash = await connection.requestAirdrop(p, (amt)*(10**9));
-console.log(`hash: ${hash}`);
+			// 1e9 lamports = 10^9 lamports = 1 SOL
+			let txhash = await connection.requestAirdrop(publicKeyObject, 1e9);
+			console.log(`txhash: ${txhash}`);
   
 };
 
